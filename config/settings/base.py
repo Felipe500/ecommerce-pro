6cff6e53b6e2db9ee09adb1ca9d408fefbf6b50f
felipe <felipe.brx.dev@gmail.com>
2024-11-30 22:57:46 -0300

    add alter config

--- config/settings/base.py
+++ config/settings/base.py
@@ -149,9 +149,3 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
